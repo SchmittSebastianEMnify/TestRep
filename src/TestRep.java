68e30d1aaef9b7125c0b343f20cbdbc6e0770acb
SchmittSebastianEMnify <Sebastian.Schmitt@EMnify.com>
2015-01-12 12:51:33 +0100

    hotfix

--- src/TestRep.java
+++ src/TestRep.java
@@ -9,10 +9,15 @@ public class TestRep {
     print("Test Feature 2");
 
     print("Feature2 Branch Test");
+    hotfixprint("Hotfix");
   }
 
 
   public static void print(String input) {
+    System.out.println(input);
+  }
+  
+  public static void hotfixprint(String input) {
     System.out.println(input);
   }
 }
